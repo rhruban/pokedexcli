3d0e6cb52ab6b51cfa6cbba3c6acd3870440976e
rhruban <rhruban@gmail.com>
2026-01-27 14:52:51 -0700

    started repl loop

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import(
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main() {
-	message := "Hello, World!"
-	fmt.Print(message)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		cleanInput := cleanInput(userInput)
+		fmt.Printf("Your command was: %s\n", cleanInput[0])
+	}
 }
